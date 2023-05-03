d8b4ec807ff0118f3afd3259ef01b00ab776f795
MohammedFairoz1 <114471735+MohammedFairoz1@users.noreply.github.com>
2023-05-03 13:20:34 +0530

    Delete regular_assignment/week6 directory

--- regular_assignment/week6/W6D2/GetterSetterProductMain.java
+++
@@ -1,9 +0,0 @@
-public class GetterSetterProductMain{
-  public static void main(String args[]){
-    Product p=new Product();
-    p.setPid(10);
-    p.setCost(20.25f);
-    p.setName("siraj");
-    System.out.println(p.getPid()+" "+p.getCost()+" "+p.getName());
-  }  
-}
